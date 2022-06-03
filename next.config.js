e723cf09c42bb9d07742d17c49c4fea0285dc2ea
Gabriel Micah <gabrielsuave17@gmail.com>
2022-06-04 00:00:26 +0100

    setup project base

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const dotenv = require('dotenv');
 
-module.exports = nextConfig
+const nextConfig = {
+  // env: {
+  //   BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  //   UPLOAD_CARE_PUBLIC_KEY: process.env.NEXT_PUBLIC_UPLOAD_CARE_PUBLIC_KEY,
+  //   STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
+  //   SENDGRID_API_KEY: process.env.NEXT_PUBLIC_SENDGRID_API_KEY,
+  // },
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com', 'ucarecdn.com'],
+  },
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
