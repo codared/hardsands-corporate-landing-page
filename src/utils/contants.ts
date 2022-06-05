66cb5ce2904ea350a99780e36448c54157889d8f
Gabriel Micah <gabrielsuave17@gmail.com>
2022-06-05 11:20:57 +0100

    Update contants.ts

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,6 +1,6 @@
 export const BRAND_DESCRIPTION = `One time Business Card for your team, business and personal use.
 All you have to do it tap on the device with the Hardsands card to share your
-profile, business, bank details or even social media links.
+profile, business, bank details or even social media links. All card variants are customizable.
 Just tap it!`;
 
 export const BRAND_TITLE = `Hardsands - One time Business Card`;
