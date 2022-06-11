000730f608ba684e90c0145e2dd5d1462c417722
Gabriel Micah <gabrielsuave17@gmail.com>
2022-06-11 04:20:04 +0100

    Update ui.tsx

--- src/pages/ui.tsx
+++ src/pages/ui.tsx
@@ -13,50 +13,10 @@ import { HomePage } from "modules/hardsands";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 import React from "react";
 
-// function DrawerExample() {
-//   const { isOpen, onOpen, onClose } = useDisclosure();
-//   const btnRef = React.useRef();
-
-//   return (
-//     <>
-//       {/* @ts-ignore */}
-//       <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
-//         Open
-//       </Button>
-//       <Drawer
-//         isOpen={isOpen}
-//         placement="right"
-//         onClose={onClose}
-//         // @ts-ignore
-//         finalFocusRef={btnRef}
-//       >
-//         <DrawerOverlay>
-//           <DrawerContent>
-//             <DrawerCloseButton />
-//             <DrawerHeader>Create your account</DrawerHeader>
-
-//             <DrawerBody>
-//               <Input placeholder="Type here..." />
-//             </DrawerBody>
-
-//             <DrawerFooter>
-//               <Button variant="outline" mr={3} onClick={onClose}>
-//                 Cancel
-//               </Button>
-//               <Button colorScheme="blue">Save</Button>
-//             </DrawerFooter>
-//           </DrawerContent>
-//         </DrawerOverlay>
-//       </Drawer>
-//     </>
-//   );
-// }
-
 const UIPage = () => {
   return (
     <ParallaxProvider>
       <Navigation />
-      {/* <Drawer /> */}
       <Container maxW="container.lg">
         <SimpleGrid columns={[2, 3, 4]} spacing={[2, 3, 5]}>
           <ProductCard />
