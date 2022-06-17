4f96ce31883de628a4a688371cb098b3be12397f
simidawalang <simidawalang@gmail.com>
2022-06-17 03:17:19 +0100

    built contact page

---
+++ src/design/index.ts
@@ -0,0 +1,9 @@
+import locationIcon from "./svg/location_bg.svg";
+import mailIcon from "./svg/mail_bg.svg";
+import phoneIcon from "./svg/phone_us_icon.svg"
+
+export {
+    locationIcon,
+    mailIcon,
+    phoneIcon
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -10,9 +10,10 @@ import {
   Flex,
   Image,
   Text,
-  Stack,
+  Button,
+  Textarea,
 } from "@chakra-ui/react";
-import mailIcon from "design/svg/mail_bg.svg";
+import { locationIcon, mailIcon, phoneIcon } from "design";
 
 const Contact: NextPage = () => {
   return (
@@ -31,23 +32,50 @@ const Contact: NextPage = () => {
         p={["3rem", "4rem 2rem", "4rem 12rem"]}
         flexDir={["column", "row"]}
       >
-        <Flex mt="10">
-          <Box>
-            <Heading mb="60px">Get In Touch</Heading>
-            <Flex alignItems="center" gap={5} mb={5}>
-              <Image h="54px" src={mailIcon.src} />
-              <Text>+2348975869676</Text>
-            </Flex>
-            <Flex alignItems="center" gap={5} mb={5}>
-              <Image h="54px" src={mailIcon.src} />
-              <Text>hello@hardsand.com</Text>
-            </Flex>
-            <Flex alignItems="center" gap={5} mb={5}>
-              <Image h="54px" src={mailIcon.src} />
-              <Text>No. 5, Street Address, State, Country</Text>
-            </Flex>
+        <Box flex={1}>
+          <Heading mb="60px">Get In Touch</Heading>
+          <Flex alignItems="center" gap={5} mb={5}>
+            <Image h="54px" src={phoneIcon.src} />
+            <Text>+2348975869676</Text>
+          </Flex>
+          <Flex alignItems="center" gap={5} mb={5}>
+            <Image h="54px" src={mailIcon.src} />
+            <Text>hello@hardsand.com</Text>
+          </Flex>
+          <Flex alignItems="center" gap={5} mb={5}>
+            <Image h="54px" src={locationIcon.src} />
+            <Text>No. 5, Street Address, State, Country</Text>
+          </Flex>
+        </Box>
+        <FormControl flex={1} boxShadow="sm" padding={5}>
+          <Grid templateColumns={["", "repeat(2, 1fr)"]} gap={6} mb={5}>
+            <Box>
+              <FormLabel htmlFor="firstname" fontSize="smaller">
+                First Name
+              </FormLabel>
+              <Input id="firstname" type="text" placeholder="Your First Name" />
+            </Box>
+            <Box>
+              <FormLabel htmlFor="lastname" fontSize="smaller">
+                Last Name
+              </FormLabel>
+              <Input id="lastname" type="text" placeholder="Your Last Name" />
+            </Box>
+          </Grid>
+          <Box mb={5}>
+            <FormLabel htmlFor="email" fontSize="smaller">
+              Email
+            </FormLabel>
+            <Input id="email" type="email" placeholder="Your Email Address" />
           </Box>
-        </Flex>
+          <Box mb={5}>
+            <FormLabel htmlFor="message">Message</FormLabel>
+            <Textarea id="message" placeholder="Your message" rows={8} />
+          </Box>
+          <Button bg="brand.300" w="full">
+            Send Message
+          </Button>
+        </FormControl>
       </Flex>
     </WithLayout>
   );
