c51a8a0588559881443833b2b3a61f8fc04f42cb
simidawalang <simidawalang@gmail.com>
2022-06-17 03:20:53 +0100

    removed dummy data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,46 @@ import {
   Button,
 } from "@chakra-ui/react";
 import ProductCard from "components/ProductCard";
-import { products } from "dummy_data/products";
+
+const products = [
+  {
+    id: 0,
+    name: "hardsands metal card",
+    description: "Our most exclusive product premium, innovative, bespoke.",
+    price: "78,000"
+  },
+  {
+    id: 1,
+    name: "hardsands metal card",
+    description: "Our most exclusive product premium, innovative, bespoke.",
+    price: "78,000"
+  },
+  {
+    id: 2,
+    name: "hardsands metal card",
+    description: "Our most exclusive product premium, innovative, bespoke.",
+    price: "78,000"
+  },
+  {
+    id: 3,
+    name: "hardsands metal card",
+    description: "Our most exclusive product premium, innovative, bespoke.",
+    price: "78,000"
+  },
+  {
+    id: 4,
+    name: "hardsands metal card",
+    description: "Our most exclusive product premium, innovative, bespoke.",
+    price: "78,000"
+  },
+  {
+    id: 5,
+    name: "hardsands metal card",
+    description: "Our most exclusive product premium, innovative, bespoke.",
+    price: "78,000"
+  },
+];
+
 
 const Home: NextPage = () => {
   return (
