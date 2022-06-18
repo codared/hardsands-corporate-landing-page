e4d7d9de5b5f934b4da504f7653a7ddfa3a02a23
Gabriel Micah <gabrielsuave17@gmail.com>
2022-06-18 04:49:57 +0100

    Update index.tsx

--- src/modules/hardsands/components/HomePage/index.tsx
+++ src/modules/hardsands/components/HomePage/index.tsx
@@ -1,5 +1,32 @@
 const HomePage = () => {
-    return (<>Hello world</>);
+  return (
+    <Box>
+      <ParallaxProvider>
+        {/* Hero Section */}
+        <HeroSection />
+        {/* End Hero Section */}
+
+        {/* Share your business Section */}
+        <ShareYourBusinessSection />
+        {/* End Share your business Section */}
+
+        {/* Take Control Section */}
+        <TakeControlSection />
+        {/* End Take Control Section */}
+
+        {/* Usage Demo Section */}
+        <UsageDemoSection />
+        {/* End Usage Demo Section */}
+
+        {/* Product Showcase Section */}
+        <ProductShowcaseSection />
+        {/* End Product Showcase Section */}
+
+        {/* What Customers are saying Section */}
+        {/* End What Customers are saying Section */}
+      </ParallaxProvider>
+    </Box>
+  );
 };
 
 export default HomePage;
