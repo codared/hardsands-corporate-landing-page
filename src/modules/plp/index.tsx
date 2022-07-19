655d9e056d39b9b68cd0364891a46334f0421d1b
Gabriel Micah <gabrielsuave17@gmail.com>
2022-07-19 13:20:39 +0100

    Update index.tsx

--- src/modules/plp/index.tsx
+++ src/modules/plp/index.tsx
@@ -1,5 +1,5 @@
 import { Grid, Flex, Image, Text } from "@chakra-ui/react";
-import { ProductCard } from "components/ProductCard";
+import ProductCard from "components/ProductCard";
 import MiniSteps from "modules/hardsands/components/miniStepsBanner";
 
 const ShopPLP = () => {
