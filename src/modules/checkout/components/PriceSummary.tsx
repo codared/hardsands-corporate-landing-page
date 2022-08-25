0f688f7712cfbf6dcc39d31911cde67e3a18f022
Gabriel Micah <gabrielsuave17@gmail.com>
2022-08-25 03:40:40 +0100

    Update PriceSummary.tsx

--- src/modules/checkout/components/PriceSummary.tsx
+++ src/modules/checkout/components/PriceSummary.tsx
@@ -71,7 +71,7 @@ const PriceSummary = ({
           {t("checkout:total", "Total:")}
         </Text>
         <Text fontSize={[fontSize - 4, fontSize]} fontWeight="bolder">
-          {formatCurrencyInteger(total, currency)}
+          {formatCurrencyInteger(total + shippingSelected.price, currency)}
         </Text>
       </Flex>
     </Box>
