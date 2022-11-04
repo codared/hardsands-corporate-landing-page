e94aadfcb391cd03cd6f72320bb5398ffbdabe27
Gabriel Micah <gabrielsuave17@gmail.com>
2022-11-04 13:44:17 +0100

    Update useAuthentication.ts

--- src/hooks/useAuthentication.ts
+++ src/hooks/useAuthentication.ts
@@ -1,7 +1,9 @@
-import { HARDSANDS_LOGIN_COOKIE, APP_ROUTE } from "modules/authentication/constants";
+import {
+  HARDSANDS_LOGIN_COOKIE,
+  APP_ROUTE,
+} from "modules/authentication/constants";
 import { getCookie } from "modules/shared/cookie";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { isTokenExpired } from "utils/functions";
 
 const useAuthentication = () => {
@@ -9,11 +11,9 @@ const useAuthentication = () => {
   const cookie = getCookie(HARDSANDS_LOGIN_COOKIE);
   const _isTokenExpired = isTokenExpired(cookie as string);
 
-  useEffect(() => {
-    if (!_isTokenExpired) {
-      router.push(APP_ROUTE.home);
-    }
-  }, [_isTokenExpired, router]);
+  if (!_isTokenExpired) {
+    router.push(APP_ROUTE.home);
+  }
 
   return;
 };
