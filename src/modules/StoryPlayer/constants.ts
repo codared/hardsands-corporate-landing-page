0ede220970d4509a13e941632504661d625e4572
Gabriel Micah <gabrielsuave17@gmail.com>
2023-02-17 18:08:14 +0100

    try to fix on all story end crash

--- src/modules/StoryPlayer/constants.ts
+++ src/modules/StoryPlayer/constants.ts
@@ -1,15 +1,17 @@
 export const PROFILES = [
   {
     id: 1,
-    image: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.46.15_PM.png?v=1676386212",
+    image:
+      "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.46.15_PM.png?v=1676386212",
     status: "Product",
     stories: [
       {
         url: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Customize-your-card.gif?v=1675103098",
-        duration: 15000,
+        duration: 10000,
         header: {
           heading: "Product",
-          profileImage: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.46.15_PM.png?v=1676386212",
+          profileImage:
+            "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.46.15_PM.png?v=1676386212",
         },
         // type: "video",
       },
@@ -17,56 +19,74 @@ export const PROFILES = [
   },
   {
     id: 2,
-    image: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.50.56_PM.png?v=1676386271",
-    status: "Reviews",
+    image:
+      "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.50.56_PM.png?v=1676386271",
+    status: "Customer Review",
     stories: [
       {
         url: "https://cdn.shopify.com/videos/c/o/v/33239f0db1aa4b4cb90eedaed868ff75.mp4",
         header: {
-          heading: "Review",
-          profileImage: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.50.56_PM.png?v=1676386271",
+          heading: "Customer Review",
+          profileImage:
+            "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.50.56_PM.png?v=1676386271",
         },
         duration: 28000,
         type: "video",
       },
-      {
-        url: "https://cdn.shopify.com/videos/c/o/v/33239f0db1aa4b4cb90eedaed868ff75.mp4",
-        header: {
-          heading: "Review",
-          profileImage: "https://cdn.shopify.com/videos/c/o/v/e4d2b1aaf0ef4724beff5c2a7df82519.mov",
-        },
-        duration: 28000,
-        type: "video",
-      },
-      {
-        url: "https://cdn.shopify.com/videos/c/o/v/bedbbb95204a433d9543494d12329b9c.mp4",
-        header: {
-          heading: "Review",
-          profileImage: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.51.47_PM.png?v=1676386322",
-        },
-        type: "video",
-      },
+      //   {
+      //     url: "https://cdn.shopify.com/videos/c/o/v/e4d2b1aaf0ef4724beff5c2a7df82519.mov",
+      //     header: {
+      //       heading: "Customer Review",
+      //       profileImage:
+      //         "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.50.56_PM.png?v=1676386271",
+      //     },
+      //     duration: 14000,
+      //     type: "video",
+      //   },
     ],
   },
   {
     id: 3,
-    image: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.53.13_PM.png?v=1676386412",
+    image:
+      "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.53.13_PM.png?v=1676386412",
     status: "How to Use",
     stories: [
       {
         url: "https://cdn.shopify.com/videos/c/o/v/40dcd31366d94eb8b92cfdb05e6e18d3.mov",
         header: {
           heading: "How to Use",
-          profileImage: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.53.13_PM.png?v=1676386412",
+          profileImage:
+            "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.53.13_PM.png?v=1676386412",
         },
         type: "video",
+        duration: 22000,
       },
+    ],
+  },
+  {
+    id: 4,
+    image:
+      "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.51.47_PM.png?v=1676386322",
+    status: "How to Use",
+    stories: [
       {
         url: "https://cdn.shopify.com/videos/c/o/v/1cd04c2ba84f41638a6be581a04207f1.mp4",
         header: {
           heading: "How to Use",
-          profileImage: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.53.13_PM.png?v=1676386412",
+          profileImage:
+            "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.53.13_PM.png?v=1676386412",
         },
+        duration: 22000,
+        type: "video",
+      },
+      {
+        url: "https://cdn.shopify.com/videos/c/o/v/bedbbb95204a433d9543494d12329b9c.mp4",
+        header: {
+          heading: "How to Use",
+          profileImage:
+            "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/Screenshot_2023-02-14_at_3.51.47_PM.png?v=1676386322",
+        },
+        duration: 74000,
         type: "video",
       },
     ],
