219a539021d64b370db6b5933cfce74788a7a500
L I L V C <simidawalang@gmail.com>
2023-03-25 12:20:39 +0100

    built card holders table

--- src/modules/account/Dashboard/Devices/[id].tsx
+++ src/modules/account/Dashboard/Devices/[id].tsx
@@ -1,41 +1,166 @@
 import { useState } from "react";
-import { Box, Text, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Heading,
+  Flex,
+  Progress,
+  Tag,
+  TagLabel,
+  useDisclosure,
+} from "@chakra-ui/react";
 import StatsCard from "../sharedComponents/StatsCard";
+import NameColumn from "../Members/components/NameColumn";
+import RowMenu from "../sharedComponents/RowMenu";
+import DataTable from "../sharedComponents/DataTable";
+
+const buildMemberRow = (members: any, rowMenuOptions: any) => {
+  return members.map((member: any) => {
+    return {
+      name: (
+        <NameColumn
+          name={member.name}
+          subText={member.email}
+          img={member.img}
+        />
+      ),
+      usage: (
+        <Flex flexDirection={["column", "row"]} alignItems={"center"}>
+          <Progress
+            rounded={"full"}
+            size={"sm"}
+            colorScheme={"orange"}
+            value={80}
+            w={"full"}
+            mr={[2]}
+          />
+          <Text>{member.usage}%</Text>
+        </Flex>
+      ),
+      clicks: member.clicks,
+      role: member.role,
+      status: (
+        <Flex justifyContent={["none", "space-between"]}>
+          <Tag
+            px={[4]}
+            py={[1]}
+            size={"sm"}
+            variant="subtle"
+            bg={member.active ? "green.400" : "gray.300"}
+            rounded={"full"}
+          >
+            <TagLabel color={"white"}>
+              {member.active ? "Active" : "Inactive"}
+            </TagLabel>
+          </Tag>
+          <RowMenu menuOption={rowMenuOptions(member)} />
+        </Flex>
+      ),
+    };
+  });
+};
 
 const DeviceWithId = () => {
-  const deviceCards = [
+  const memberDrawer = useDisclosure();
+  const createDrawer = useDisclosure();
+  const [activeMember, setActiveMember] = useState({});
+  const [drawerFormState, setDrawerFormState] = useState({
+    name: "",
+    subTitle: "",
+    form: <></>,
+  });
+  const columnHeaders = ["Name", "Card Usage", "Clicks", "Role", "Status"];
+  const dataStore = [
     {
-      id: 1,
-      img: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/14_1.jpg?v=1665057392",
-      name: "Black Matte PVC Card",
-      number: "10",
+      img: "https://bit.ly/sage-adebayo",
+      name: "John Doe",
+      email: "gab@hardsands.com",
+      usage: 80,
+      clicks: 200,
+      role: "Developer",
+      status: "Active",
     },
     {
-      id: 2,
-      img: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/12a_1.jpg?v=1665057392",
-      name: "Bamboo Wood Card",
-      number: "10",
+      img: "https://bit.ly/sage-adebayo",
+      name: "John Doe",
+      email: "gab@hardsands.com",
+      usage: 100,
+      clicks: 200,
+      role: "Developer",
+      status: "Active",
     },
     {
-      id: 3,
-      img: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/14_1.jpg?v=1665057392",
-      name: "Black Matte Metal Card",
-      number: "10",
+      img: "https://bit.ly/sage-adebayo",
+      name: "John Doe",
+      email: "gab@hardsands.com",
+      usage: 10,
+      clicks: 200,
+      role: "Developer",
+      status: "Active",
     },
     {
-      id: 4,
-      img: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/14_1.jpg?v=1665057392",
-      name: "Sapele Card",
-      number: "10",
+      img: "https://bit.ly/sage-adebayo",
+      name: "John Doe",
+      email: "gab@hardsands.com",
+      usage: 10,
+      clicks: 200,
+      role: "Developer",
+      status: "Active",
     },
     {
-      id: 5,
-      img: "https://cdn.shopify.com/s/files/1/0559/0407/5843/files/14_1.jpg?v=1665057392",
-      name: "Epoxy PVC",
-      number: "10",
+      img: "https://bit.ly/sage-adebayo",
+      name: "John Doe",
+      email: "gab@hardsands.com",
+      usage: 10,
+      clicks: 200,
+      role: "Developer",
+      status: "Active",
+    },
+    {
+      img: "https://bit.ly/sage-adebayo",
+      name: "John Doe",
+      email: "gab@hardsands.com",
+      usage: 10,
+      clicks: 200,
+      role: "Developer",
+      status: "Active",
     },
   ];
 
+  const rowMenuOptions = (member: any) => {
+    return [
+      {
+        id: 1,
+        title: "Edit Member",
+        onClick: () => {
+          setActiveMember(member);
+          memberDrawer.onOpen();
+        },
+      },
+      {
+        id: 2,
+        title: "Set Permissions",
+        onClick: () => {},
+      },
+      {
+        id: 3,
+        title: "Lock Profile",
+        onClick: () => {},
+      },
+      {
+        id: 4,
+        title: "View Report",
+        onClick: () => {},
+      },
+      {
+        id: 5,
+        title: "Remove Member",
+        onClick: () => {},
+      },
+    ];
+  };
+
+  const data = buildMemberRow(dataStore, rowMenuOptions);
   return (
     <Box>
       <Heading>Device</Heading>{" "}
@@ -57,16 +182,11 @@ const DeviceWithId = () => {
         <StatsCard name="Total Cards" number="11" />
         <StatsCard showMenu={false} name="Unassigned Cards" number="6" />
       </Box>
-      <Box mt={6} bg="#fff" py={10} px={5}>
-        <Box
-          display="grid"
-       
-          gap={8}
-        >
-          <Heading fontSize={"1.25rem"}>Card Holders</Heading>
-          
-        </Box>
-      </Box>
+      <DataTable
+        headers={columnHeaders}
+        data={data}
+        tableTitle={"Card Holders"}
+      />
     </Box>
   );
 };
