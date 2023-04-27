f725d601f3cc97a38f4909e966eaa1d6e10676b7
L I L V C <simidawalang@gmail.com>
2023-04-27 15:45:55 +0100

    refactored phone number input

--- src/modules/account/AccessMembershipDashboard/Members/components/CreateMemberForm/index.tsx
+++ src/modules/account/AccessMembershipDashboard/Members/components/CreateMemberForm/index.tsx
@@ -134,9 +134,23 @@ const CreateMemberForm = ({
           placeholder="Phone"
           name="phone"
           type="text"
-          value={values.phone}
+          maxLength={11}
+          value={values.phone.trim()}
           isRequired
           onChange={handleChange}
+          onKeyDown={(e: any) => {
+            if (
+              e.key === " " ||
+              (!Number(e.key) &&
+                e.key !== "0" &&
+                e.key !== "Backspace" &&
+                e.key !== "ArrowLeft" &&
+                e.key !== "ArrowRight")
+            ) {
+              // if the entered key is not an interger, not a zero, backspace or arrow key
+              e.preventDefault();
+            }
+          }}
         />
         <Box mb={4}>
           <FormLabel>Select Membership Tag</FormLabel>
