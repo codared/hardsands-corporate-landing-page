13121a77ea577c5f1025edc92f3e08742d16221d
Gabriel Micah <gabrielsuave17@gmail.com>
2023-06-27 17:07:12 +0100

    reset card

--- src/modules/account/Dashboard/Devices/services.ts
+++ src/modules/account/Dashboard/Devices/services.ts
@@ -1,6 +1,22 @@
 import { makeTheAuthenticatedRequest } from "modules/account/services";
+import { storefrontApiJsonFetch } from "modules/api";
+import { requestAuthHeaders } from "utils/functions";
+import { BackendResponseType } from "utils/types";
 
 export const getCards = async () => {
   const res = await makeTheAuthenticatedRequest(`/api/corporate-admin/cards`);
+  return res;
+};
+
+export const resetCardService = async (serial: string) => {
+  const res = (await storefrontApiJsonFetch(
+    `/api/corporate-admin/cards/reset/${serial}`,
+    {
+      method: "POST",
+      headers: requestAuthHeaders(),
+    }
+  )) as BackendResponseType;
+
+  return res;
   return res;
 };
--- src/modules/account/Dashboard/Members/components/MemberProfile.tsx
+++ src/modules/account/Dashboard/Members/components/MemberProfile.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Button, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import ActionsPermissions from "./ActionsPermissions";
 import CreateMemberForm from "./CreateMemberForm";
@@ -10,6 +10,8 @@ const MemberProfile = ({
   showActions = true,
   showCard = false,
   createDrawer,
+  loading,
+  handleResetCard,
 }: any) => {
   return (
     <Box>
@@ -33,6 +35,14 @@ const MemberProfile = ({
             alt={member.fullName}
           />
           <Box py={[6]}>
+            {member.cardName && (
+              <Box mt={2} mb={6}>
+                <Box fontSize={"sm"} fontWeight={"bold"}>
+                  Card Variant
+                </Box>
+                <Box fontSize={"sm"}>{member.cardName}</Box>
+              </Box>
+            )}
             <NameColumn
               name={member.fullName}
               subText={member.email}
@@ -40,6 +50,34 @@ const MemberProfile = ({
               isActive={member.isActive}
             />
           </Box>
+        </Box>
+      )}
+      {handleResetCard && typeof handleResetCard === "function" && (
+        <Box mt={10}>
+          <Box mb={4} color={"red.400"} textAlign={"left"}>
+            <Text fontWeight={"bold"} fontSize={14}>
+              Warning: Resetting a card will remove all data associated with the
+              card. This action cannot be undone.
+            </Text>
+          </Box>
+
+          <Button
+            onClick={handleResetCard}
+            cursor={"pointer"}
+            color={"red.400"}
+            fontWeight={"bold"}
+            fontSize={"sm"}
+            p={2}
+            bg={"red.50"}
+            textAlign={"center"}
+            borderRadius={"none"}
+            _hover={{ bg: "red.100", color: "black" }}
+            w={"full"}
+            loadingText={"Resetting Card..."}
+            isLoading={loading}
+          >
+            Reset Card
+          </Button>
         </Box>
       )}
     </Box>
