d3fe084245eb75e148d083b22583c10ee5d101d8
Ohue James <ohuejames101@gmail.com>
2023-08-13 00:23:06 +0100

    mobile2.4

--- src/pages/corporate.tsx
+++ src/pages/corporate.tsx
@@ -1871,8 +1871,8 @@ const Corporate = () => {
                     textTransform="capitalize"
                     textAlign={["center", "center", "inherit"]}
                   >
-                    Can&apos;'t find the answers you&apos;'re looking for?
-                    Contact us via our Hardsands Technology Customer Support
+                    Can&apos;t find the answers you&apos;re looking for? Contact
+                    us via our Hardsands Technology Customer Support
                   </Text>
                 </Text>
                 <Box w={["100%", "", "17%"]}>
